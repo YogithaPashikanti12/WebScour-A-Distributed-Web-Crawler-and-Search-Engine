c20455a9a0fc3ac94c34fd5afdab0b693982eda6
YogithaPashikanti12 <yogitha.pashikanti@gmail.com>
2026-03-13 20:27:15 +0530

    Add files via upload

---
+++ Milestone4/producer.py
@@ -0,0 +1,35 @@
+# producer.py
+
+import pika
+
+# Seed URLs
+SEED_URLS = [
+    "https://www.python.org",
+    "https://books.toscrape.com"
+]
+
+# Connect to RabbitMQ
+connection = pika.BlockingConnection(
+    pika.ConnectionParameters('localhost')
+)
+
+channel = connection.channel()
+
+# Declare durable queue
+channel.queue_declare(queue='url_queue', durable=True)
+
+# Publish seed URLs
+for url in SEED_URLS:
+    channel.basic_publish(
+        exchange='',
+        routing_key='url_queue',
+        body=url,
+        properties=pika.BasicProperties(
+            delivery_mode=2,  # Make message persistent
+        )
+    )
+    print(f"[Producer] Sent: {url}")
+
+print("All seed URLs sent.")
+
+connection.close()
---
+++ Milestone4/search_engine.py
@@ -0,0 +1,66 @@
+import json
+import re
+from collections import defaultdict
+
+# -------------------------
+# Load index data
+# -------------------------
+with open("inverted_index.json") as f:
+    inverted_index = json.load(f)
+
+with open("idf.json") as f:
+    idf = json.load(f)
+
+
+# -------------------------
+# Query Tokenization
+# -------------------------
+def tokenize_query(query):
+    query = query.lower()
+    query = re.sub(r"[^a-z0-9\s]", "", query)
+    tokens = query.split()
+    return tokens
+
+
+# -------------------------
+# Search Function
+# -------------------------
+def search(query, top_n=5):
+
+    tokens = tokenize_query(query)
+
+    scores = defaultdict(float)
+
+    for word in tokens:
+
+        if word in inverted_index:
+
+            postings = inverted_index[word]
+
+            for doc_id, tf in postings:
+
+                score = tf * idf.get(word, 0)
+
+                scores[doc_id] += score
+
+    # sort documents by score
+    ranked_docs = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+
+    return ranked_docs[:top_n]
+
+
+# -------------------------
+# Test search in terminal
+# -------------------------
+if __name__ == "__main__":
+
+    while True:
+
+        query = input("Enter search query: ")
+
+        results = search(query)
+
+        print("\nResults:")
+
+        for doc, score in results:
+            print(doc, "Score:", round(score, 3))
